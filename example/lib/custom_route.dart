f351ddc0928cc27a42fdbb12f5b3c970c4f122f5
whitevoid <381848900@qq.com>
2020-03-22 11:37:19 +0800

    fix: Fix flushbar dependent address

--- example/lib/custom_route.dart
+++ example/lib/custom_route.dart
@@ -18,10 +18,6 @@ class FadePageRoute<T> extends MaterialPageRoute<T> {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    if (settings.isInitialRoute) {
-      return child;
-    }
-
     return FadeTransition(
       opacity: animation,
       child: child,
