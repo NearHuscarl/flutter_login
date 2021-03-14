ff327116dc898d11c7e4aaa96ec18a43a4e2bd1f
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-03-14 22:00:15 +0100

    Updated deprecated text parameters

--- example/lib/dashboard_screen.dart
+++ example/lib/dashboard_screen.dart
@@ -150,7 +150,7 @@ class _DashboardScreenState extends State<DashboardScreen>
               children: <Widget>[
                 Text(
                   '\$',
-                  style: theme.textTheme.display2.copyWith(
+                  style: theme.textTheme.headline3.copyWith(
                     fontWeight: FontWeight.w300,
                     color: accentColor.shade400,
                   ),
@@ -161,7 +161,7 @@ class _DashboardScreenState extends State<DashboardScreen>
                   targetValue: 3467.87,
                   curve: Interval(0, .5, curve: Curves.easeOut),
                   controller: _loadingController,
-                  style: theme.textTheme.display2.copyWith(
+                  style: theme.textTheme.headline3.copyWith(
                     foreground: Paint()..shader = linearGradient,
                   ),
                 ),
@@ -266,7 +266,7 @@ class _DashboardScreenState extends State<DashboardScreen>
       right: 0,
       child: Row(
         children: <Widget>[
-          RaisedButton(
+          MaterialButton(
             materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
             color: Colors.red,
             child: Text('loading', style: textStyle),
--- lib/src/widgets/auth_card.dart
+++ lib/src/widgets/auth_card.dart
@@ -575,7 +575,7 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
       child: FlatButton(
         child: Text(
           messages.forgotPasswordButton,
-          style: theme.textTheme.body1,
+          style: theme.textTheme.bodyText2,
           textAlign: TextAlign.left,
         ),
         onPressed: buttonEnabled
@@ -829,7 +829,7 @@ class _RecoverCardState extends State<_RecoverCard>
                   messages.recoverPasswordIntro,
                   key: kRecoverPasswordIntroKey,
                   textAlign: TextAlign.center,
-                  style: theme.textTheme.body1,
+                  style: theme.textTheme.bodyText2,
                 ),
                 SizedBox(height: 20),
                 _buildRecoverNameField(textFieldWidth, messages, auth),
@@ -838,7 +838,7 @@ class _RecoverCardState extends State<_RecoverCard>
                   messages.recoverPasswordDescription,
                   key: kRecoverPasswordDescriptionKey,
                   textAlign: TextAlign.center,
-                  style: theme.textTheme.body1,
+                  style: theme.textTheme.bodyText2,
                 ),
                 SizedBox(height: 26),
                 _buildRecoverButton(theme, messages),
