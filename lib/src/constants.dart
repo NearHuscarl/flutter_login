d187c42b3a5766dfa6bfdf284a6f25ed4d1e0480
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-03-30 11:32:12 +0200

    refactor: change min logo height to 50

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -5,5 +5,5 @@ const kRecoverPasswordIntroKey = Key('RECOVER_PASSWORD_INTRO');
 const kRecoverPasswordDescriptionKey = Key('RECOVER_PASSWORD_DESCRIPTION');
 const kDebugToolbarKey = Key('DEBUG_TOOLBAR');
 
-const kMinLogoHeight = 100.0; // hide logo if less than this
+const kMinLogoHeight = 50.0; // hide logo if less than this
 const kMaxLogoHeight = 125.0;
