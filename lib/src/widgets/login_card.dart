23dcedaffb2becf60c98cea58ea2a53e08c1a7dd
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-07-26 14:42:14 +0200

    style: formatting

--- lib/src/widgets/login_card.dart
+++ lib/src/widgets/login_card.dart
@@ -350,9 +350,10 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
 
   Widget _buildSwitchAuthButton(ThemeData theme, LoginMessages messages,
       Auth auth, LoginTheme loginTheme) {
-    final calculatedTextColor = (theme.cardTheme.color!.computeLuminance() < 0.5)
-        ? Colors.white
-        : theme.primaryColor;
+    final calculatedTextColor =
+        (theme.cardTheme.color!.computeLuminance() < 0.5)
+            ? Colors.white
+            : theme.primaryColor;
     return FadeIn(
       controller: _loadingController,
       offset: .5,
