8e04b97fd9cdbb08d00b4f0bc6e8a2940cc2591d
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-07-26 09:19:51 +0200

    bug: fixed switch-auth button not coloring correctly

--- lib/src/widgets/login_card.dart
+++ lib/src/widgets/login_card.dart
@@ -350,7 +350,7 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
 
   Widget _buildSwitchAuthButton(ThemeData theme, LoginMessages messages,
       Auth auth, LoginTheme loginTheme) {
-    final calculatedTextColor = (theme.backgroundColor.computeLuminance() < 0.5)
+    final calculatedTextColor = (theme.cardTheme.color!.computeLuminance() < 0.5)
         ? Colors.white
         : theme.primaryColor;
     return FadeIn(
